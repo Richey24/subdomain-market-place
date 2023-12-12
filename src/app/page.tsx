08a287aa155903669e0f3f5225945ce26bb823f3
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2023-12-12 04:04:17 +0100

    fix(texts): updated texts from mywebstore.black to blackowned.store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                 className="font-bold md:text-[3.5rem] appear-animation animated fadeInUpShorter appear-animation-visible"
                 style={{ animationDelay: "600ms" }}
               >
-                People Already Using {"'Mywebstore.black'"} Store Design
+                People Already Using {"'Blackowned.store'"} Store Design
                 Technology
               </span>
             </h2>
@@ -42,7 +42,7 @@ export default function Home() {
               className="font-bold text-md text-gray-500 text-center appear-animation animated fadeInUpShorter appear-animation-visible"
               style={{ animationDelay: "750ms" }}
             >
-              100K+ IN ALL {"Mywebstore.black.'S TECHNOLOGY'S"} VERSIONS
+              100K+ IN ALL {"Blackowned.store.'S TECHNOLOGY'S"} VERSIONS
             </p>
             <div
               className="appear-animation animated fadeInUpShorter appear-animation-visible"
@@ -86,8 +86,8 @@ export default function Home() {
                     </span>
                   </div>
                   <p className="mb-0">
-                    {'"Working'} with IMarket Place has been an absolute pleasure.
-                    Their eCommerce platform provided an intuitive and
+                    {'"Working'} with IMarket Place has been an absolute
+                    pleasure. Their eCommerce platform provided an intuitive and
                     user-friendly interface, making it incredibly easy for me to
                     set up my online store. The seamless integration with
                     various payment gateways allowed me to offer a secure and
